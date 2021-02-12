720c9527d810483841003d435b0e4d995b2c359a
elias-lousseief <elias.lousseief@schibsted.com>
2021-02-12 22:13:10 +0100

    0.0.4

--- src/MathJax.test.tsx
+++ src/MathJax.test.tsx
@@ -1,13 +1,11 @@
 // @ts-ignore
-import React from 'react'
-import {render} from "@testing-library/react";
+import React from "react"
+import { render } from "@testing-library/react"
 
 beforeAll(() => {})
 
 it("test", async () => {
-    const { queryByText, unmount } = render(
-        <div />
-    )
+    const { queryByText, unmount } = render(<div />)
     expect(queryByText("null")).toBeNull()
     unmount()
-}, 15000);
+}, 15000)
